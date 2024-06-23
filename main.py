05973c1cc058cb0cad772051a27ea4635c630616
Mohammad <mhamdl2amen@gmail.com>
2024-06-24 00:33:01 +0300

    we saving data on google sheet

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
+from datetime import datetime
 GENDER = "type your gender here"
-WEIGHT_KG = 0
-HEIGHT_CM = 0
-AGE = 0
-APP_ID="2c86f591"
-APP_KEY="2860f3e210b7f55271645b7bc1c9f088"
-natural_language_endpoint="https://trackapi.nutritionix.com/v2/natural/exercise"
+WEIGHT_KG = 70
+HEIGHT_CM = 181
+AGE = 23
+APP_ID="xxxxxx"
+APP_KEY="xxxxxxxxxx"
+natural_language_endpoint="xxxxxxxxxxxxxx"
 headers={
     "x-app-id":APP_ID,
     "x-app-key":APP_KEY,
@@ -19,4 +20,21 @@ params={
     ,"age":AGE
 }
 response=requests.post(url=natural_language_endpoint,json=params,headers=headers)
-print(response.text)
+result=response.json()
+
+today_date=datetime.now().strftime("%d/%m/%Y")
+now_time=datetime.now().strftime("%X")
+sheety_endpoint="xxxxxxxxxxxxxxxxx"
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+sheety_response=requests.post(sheety_endpoint, json=sheet_inputs)
+print(sheety_response.text)
