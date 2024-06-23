3102dd0ef6d50defd352130b85ad01006b689522
Mohammad <mhamdl2amen@gmail.com>
2024-06-24 01:04:30 +0300

    Authenticate My Sheety API

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ GENDER = "type your gender here"
 WEIGHT_KG = 70
 HEIGHT_CM = 181
 AGE = 23
-APP_ID="xxxxxx"
-APP_KEY="xxxxxxxxxx"
-natural_language_endpoint="xxxxxxxxxxxxxx"
+APP_ID="xxxxxxxx"
+APP_KEY="xxxxxxxxxxxxx"
+natural_language_endpoint="https://trackapi.nutritionix.com/v2/natural/exercise"
 headers={
     "x-app-id":APP_ID,
     "x-app-key":APP_KEY,
@@ -24,7 +24,7 @@ result=response.json()
 
 today_date=datetime.now().strftime("%d/%m/%Y")
 now_time=datetime.now().strftime("%X")
-sheety_endpoint="xxxxxxxxxxxxxxxxx"
+sheety_endpoint="xxxxxxxxxxxxxxxxxxxxx"
 for exercise in result["exercises"]:
     sheet_inputs = {
         "workout": {
@@ -35,6 +35,8 @@ for exercise in result["exercises"]:
             "calories": exercise["nf_calories"]
         }
     }
-
-sheety_response=requests.post(sheety_endpoint, json=sheet_inputs)
+headers={
+    "Authorization":"Basic Ym9vZGk6YWV6YWttaTEyMw=="
+}
+sheety_response=requests.post(sheety_endpoint, json=sheet_inputs, headers=headers)
 print(sheety_response.text)
